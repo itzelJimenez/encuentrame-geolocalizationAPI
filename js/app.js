66f891b6e9bca2239ab7905b612275140ce2bdb1
itzel <itzeljimenez.do@gmail.com>
2017-05-28 12:15:13 -0500

    determinación estática de ubicación

--- js/app.js
+++ js/app.js
@@ -15,3 +15,33 @@ function initMap(){
     });
   }
   
+function buscar(){
+  if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+  }
+}
+
+
+var funcionExito = function(posicion){
+  var latitud,longitud;
+  latitud = posicion.coords.latitude;
+  longitud = posicion.coords.longitude; 
+  console.log(latitud, longitud);
+  var mapa= new google.maps.Map(document.getElementById("map"),{
+       zoom:18,
+        center: {lat:latitud, lng:longitud}
+      });
+  var miUbicación = new google.maps.Marker({
+    position: {lat:latitud, lng:longitud},
+    map: mapa,
+    title: "My ubicación",
+    draggable: true,
+  });
+  
+};
+
+var funcionError = function(error){
+  alert("Tenemos un problema con encontrar tu ubicación.");
+};
+
+document.getElementById("encuentrame").addEventListener("click", buscar);
